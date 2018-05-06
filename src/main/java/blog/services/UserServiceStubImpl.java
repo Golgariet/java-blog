97010704fe66a706a8a03c278fa3687c9a57ae30
Anton Bibergal <nathgilt@gmail.com>
2018-05-07 01:45:12 +0300

    First commit

---
+++ src/main/java/blog/services/UserServiceStubImpl.java
@@ -0,0 +1,41 @@
+package blog.services;
+
+import blog.models.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class UserServiceStubImpl implements UserService {
+    @Override
+    public boolean authenticate(String username, String password) {
+        // Provide a sample password check: username == password
+        return Objects.equals(username, password);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User create(User user) {
+        return null;
+    }
+
+    @Override
+    public User edit(User user) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
