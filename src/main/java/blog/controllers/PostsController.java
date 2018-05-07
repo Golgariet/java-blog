fce48dd01c882852820eb28220387b8f7a6f1174
Antons Bibergals <antons.bibergals@accenture.com>
2018-05-07 15:10:17 +0300

    Database is half-done

--- src/main/java/blog/controllers/PostsController.java
+++ src/main/java/blog/controllers/PostsController.java
@@ -1,13 +1,19 @@
 package blog.controllers;
 
+import blog.forms.PostForm;
 import blog.models.Post;
 import blog.services.NotificationService;
 import blog.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class PostsController {
@@ -26,5 +32,28 @@ public class PostsController {
         }
         model.addAttribute("post", post);
         return "posts/view";
+    }
+
+    @RequestMapping("/posts/create")
+    public String CreatePostForm(PostForm PostForm){
+        return "posts/create";
+    }
+
+    @RequestMapping(value = "/posts/create", method = RequestMethod.POST)
+    public String createPostPage(@Valid PostForm PostForm, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            notifyService.addErrorMessage("Please fill the form correctly!");
+            return "posts/create";
+        }
+
+        notifyService.addInfoMessage("Post created successfully!");
+        return "redirect:/";
+    }
+
+    @RequestMapping("/posts")
+    public String posts(Model model){
+        List<Post> allPosts = postService.findAll();
+        model.addAttribute("allPosts", allPosts);
+        return "posts/posts";
     }
 }
--- src/main/java/blog/controllers/RegistrationController.java
+++ src/main/java/blog/controllers/RegistrationController.java
@@ -1,6 +1,6 @@
 package blog.controllers;
 
-import blog.forms.LoginForm;
+import blog.forms.RegistrationForm;
 import blog.services.NotificationService;
 import blog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,33 +12,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.validation.Valid;
 
 @Controller
-public class LoginController {
-
+public class RegistrationController {
     @Autowired
     private UserService userService;
 
     @Autowired
     private NotificationService notifyService;
 
-    @RequestMapping("/users/login")
-    public String login(LoginForm loginForm) {
-        return "users/login";
+    @RequestMapping(value="/users/register")
+    public String register(RegistrationForm registrationForm){
+        return "users/register";
     }
 
-    @RequestMapping(value = "/users/login", method = RequestMethod.POST)
-    public String loginPage(@Valid LoginForm loginForm, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
+    @RequestMapping(value= "/users/register", method = RequestMethod.POST)
+    public String registerPage(@Valid RegistrationForm registrationForm, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
             notifyService.addErrorMessage("Please fill the form correctly!");
-            return "users/login";
+            return "users/register";
         }
 
-        if (!userService.authenticate(
-                loginForm.getUsername(), loginForm.getPassword())) {
-            notifyService.addErrorMessage("Invalid login!");
-            return "users/login";
-        }
-
-        notifyService.addInfoMessage("Login is successful");
+        notifyService.addInfoMessage("Registration is successful");
         return "redirect:/";
     }
 }
